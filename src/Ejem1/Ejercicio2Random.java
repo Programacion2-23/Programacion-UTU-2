ca7382291559eebb07f65852dc6dec2759d93042
Alejo <alejo12arias@gmail.com>
2023-03-19 20:02:21 -0300

    Mas Proyectos

---
+++ src/Ejem1/Ejercicio2Random.java
@@ -0,0 +1,61 @@
+package Ejem1;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Ejercicio2Random {
+
+	void iniciar () {
+		
+		int valor = generarAleatorio();
+		int num;
+		String resul;
+		
+		do {
+			num = pedirNumero();
+			resul=compararNumeros(num, valor);
+			System.out.println(resul);
+		} while(resul!= "es igual");
+		System.out.println("Felicitaciones Acertastee!!");
+		
+	}
+	
+	
+	//pide un nro por pantalla
+		int pedirNumero() {
+			Scanner entrada = new Scanner(System.in);
+			
+			System.out.println("Ingrese un numero: ");
+			int numeroIng = entrada.nextInt();
+			
+			return numeroIng;
+		}
+	
+	//devuelve un numero aleatorio entre 1 y 1000
+	int generarAleatorio() {
+		Random rand = new Random();
+		int numR = 1000;
+		
+		int randomNumer = rand.nextInt(numR);
+		
+		return randomNumer;
+	}
+	
+	
+	//compara dos enteros a y b. Devuelve un String indicando si es menor, mayor o igual
+	 String compararNumeros(int a, int b) {
+		 String mayor = "es mayor";
+		 String menor = "es menor";
+		 String igual = "es igual";
+		 
+		 if (a > b) {
+			return mayor; 
+		 } else if (a < b) {
+			return menor;
+		} else {
+			return igual;
+		}
+		 
+		 
+	}
+	
+}
--- src/Ejem1/Principal.java
+++ src/Ejem1/Principal.java
@@ -4,6 +4,11 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		//Ejercicio1Scanner ejer1 = new Ejercicio1Scanner();
+		//ejer1.inicar();
+		
+		Ejercicio2Random ejer2=new Ejercicio2Random();
+		ejer2.iniciar();
 	}
 
 }
